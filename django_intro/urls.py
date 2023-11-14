3d50dd35588ae82c811448899108b4b33e284c2a
ompangyji <ompangyji@gmail.com>
2023-11-14 16:35:47 +0900

    urls.py에 url 등록

--- django_intro/urls.py
+++ django_intro/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_intro import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('index/', views.index),
 ]
